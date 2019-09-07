6869252d9bf1af3c7ab0ebbeb1c88bb035bdfc56
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2019-09-06 19:13:45 -0500

    minor edits

--- Data/Eliteprospects Data/Eliteprospects data Collection.R
+++ Data/Eliteprospects Data/Eliteprospects data Collection.R
@@ -1,19 +1,16 @@
 require(data.table)
 require(here)
 
-#Eliteprospects scrapping ----
-
-
-## Drafts
+## Drafts ----
 base_str <- 'https://www.eliteprospects.com/draft/nhl-entry-draft/'
 
 for(year in 1980:2019) {
   website <- paste0(base_str, year)
-  temp <- Ind_Scraper(website)
+  temp <- draft_Scraper(website)
   fwrite(temp, here("Data", "Eliteprospects Data", paste0("Eliteprospects_Draft_", year, '.csv')))
 }
 
-## undrafted players
+## undrafted players ----
 
 #We want to pull undrafted data as well to not skew our results when we build out the model
 #we will initially pull in all player data (so there will be duplicates) 
@@ -31,15 +28,3 @@ for(League in leagues) {
     
   }
 }
-
-
-url <- 'https://www.eliteprospects.com/player/29626/filip-forsberg'
-
-Parse <- read_html(url)
-#note that here, the second argument is that h1
-nodes <- html_nodes(Parse, 'h1')
-#finding the wanted index that has 'plytitle'
-desired_index <- grep('plytitle', nodes)
-#converting XML objects to the text we really actually want
-text <- html_text(nodes)
-text[desired_index]
